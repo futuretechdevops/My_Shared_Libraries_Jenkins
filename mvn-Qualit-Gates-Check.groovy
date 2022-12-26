eda37ff8dcc5f01bea2d8ec157a1a6a16d07ac1b
futuretechdevops <119612276+futuretechdevops@users.noreply.github.com>
2022-12-26 17:23:08 +0530

    Create mvn-Qualit-Gates-Check.groovy

---
+++ mvn-Qualit-Gates-Check.groovy
@@ -0,0 +1,75 @@
+def call(body) {
+    def args = [
+        // general agruments
+        url: '',
+        branch: '',
+        sonar_scanner: '',
+        sonar_server: '',
+        projectKey: '',
+        timeout_time: '',
+        timeout_unit: ''
+    ]
+    
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = args
+    body()
+    echo "INFO: ${args.url}"
+    echo "INFO: ${args.branch}"
+
+    
+    pipeline {
+        agent any
+        tools {
+            maven 'Maven-360' 
+            jdk 'JAVA-11' 
+        }
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    checkout([
+                    $class : 'GitSCM',
+                        branches: [[ name: args.branch ]],
+                        userRemoteConfigs: [[ url: args.url ]]
+                    ])
+                }
+            }
+            stage ('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+            stage ('Build') {
+                steps {
+                    sh 'mvn -Dmaven.test.failure.ignore=true install'
+                }
+                post {
+                    success {
+                        junit(
+                            allowEmptyResults: true,
+                            testResults: '**/target/surefire-reports/*.xml'
+                        )
+                    }
+                }
+            }
+            stage ('Code Quality') {
+            environment {
+                scannerHome = tool "args.sonar_scanner"
+            }
+            steps {
+                withSonarQubeEnv("args.sonar_server") {
+                    sh "${scannerHome}/bin/sonar-scanner \
+                    -D sonar.projectKey="args.projectKey" \
+                    -D sonar.exclusions=vendor/**,resources/**,**/*.java"
+                }
+            }
+            }
+            stage('SonarQube Quality Gates Check'){
+            steps {
+                timeout(time: "args.timeout_time", unit: 'args.timeout_unit') {
+                    waitForQualityGate abortPipeline: true
+                }
+            }
+            }
+        }
+    }
+}
